dca6e3962ad6d284373e61c772d861fba26645aa
NamanGupta1412 <namanguptapubg@gmail.com>
2024-03-25 20:23:13 +0530

    Card designing is done

--- Notes-Making/src/Components/Cards.jsx
+++ Notes-Making/src/Components/Cards.jsx
@@ -4,12 +4,12 @@ import { LuDownload } from "react-icons/lu";
 
 
 
-function Cards() {
+function Cards({data}) {
     return (
         <div className='relative overflow-hidden w-[25vh] h-[33vh] bg-black rounded-3xl text-white py-10 px-8'>
             <FaRegFileAlt />
             <p className='text-xs leading-tight mt-5'>
-                Lorem ipsum dolor sit amet consectetur adipisicing.
+                {data.desc}
             </p>
             <div className='footer absolute w-full bottom-0 left-0'>
                 <div className='flex items-center justify-between py-3 px-8 mb-3'>
@@ -17,9 +17,9 @@ function Cards() {
                     <LuDownload />
                 </div>
                 <div className='tag w-full py-4 bg-green-600 flex items-center justify-center'>
-                <h3 className='text-xs'>
-                    Download Now
-                </h3>
+                    <h3 className='text-xs'>
+                        Download Now
+                    </h3>
                 </div>
             </div>
         </div>
--- Notes-Making/src/Components/Foreground.jsx
+++ Notes-Making/src/Components/Foreground.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import Cards from './Cards'
 
+
+
 function Foreground() {
+
+
+const data = 
+[
+{
+  desc: "This is a Fake Data",
+  
+},
+{
+  desc: "This is a Fake Data2",
+  
+},
+{
+  desc: "This is a Fake Data3",
+  
+},
+];
   return (
     <div className='z-[3] w-full h-screen fixed top-0 left-0'>
-    <Cards />
+    {data.map((t,i)=>(
+      <Cards data={t}/>
+    ))}
+    
     </div>
   )
 }
